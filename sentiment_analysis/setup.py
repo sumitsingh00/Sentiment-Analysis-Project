66941db8a8ccaffdf40f7b437a6b2450563c89d5
sumitsingh00 <sumit00134@gmail.com>
2025-09-14 21:28:49 +0530

    make dir & Files

---
+++ sentiment_analysis/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The Sentiment Analysis Project focuses on building a machine learning (or deep learning) model to automatically classify the sentiment expressed in text data such as customer reviews, social media posts, or feedback forms. The project aims to understand whether the expressed sentiment is positive, negative, or neutral, helping businesses, researchers, and organizations make data-driven decisions.',
+    author='sumit',
+    license='MIT',
+)
